009e26af6df0f727aa2b905f9e5db47b2c9331e4
Kahabrakh <kahabrakh@indiplex.de>
2012-01-11 22:22:51 +0100

    Code cleanup and IPMAPI javadocs

--- src/de/indiplex/manager/IPMAPI.java
+++ src/de/indiplex/manager/IPMAPI.java
@@ -1,3 +1,20 @@
+/*
+ * IndiPlexManager
+ * Copyright (C) 2011 IndiPlex
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.indiplex.manager;
 
 import de.indiplex.manager.util.Config;
@@ -9,7 +26,7 @@ import org.bukkit.plugin.Plugin;
 
 /**
  *
- * @author IndiPlex
+ * @author IndiPlex <kahabrakh@indiplex.de>
  */
 public class IPMAPI {
     
@@ -17,12 +34,21 @@ public class IPMAPI {
     private Manager IPM;   
     private Config config;
     
+    /**
+     * Creates a new instance of the IPMAPI
+     * @param IPM Assigns the instance of the IndiPlex-Manager
+     * @param plugin Assigns the instance of the plugin to the IPMAPI 
+     */
     public IPMAPI(Manager IPM, Plugin plugin) {
         this.IPM = IPM;
         this.plugin = plugin;
         this.config = IPM.getIPMConfig();
     }
     
+    /**
+     * Gets the folder where you can save your things
+     * @return File Your data folder
+     */
     public File getDataFolder() {
         return getDataFolder(plugin);
     }
@@ -33,13 +59,16 @@ public class IPMAPI {
         return f;
     }
     
+    /**
+     * Gets your plugins configuration
+     * @return YamlConfiguration Your config
+     */
     public YamlConfiguration getConfig() {
         return getConfig(plugin);
     }
     
     private YamlConfiguration getConfig(Plugin plugin) {
         File f = new File(IPM.getDataFolder(), "/config/"+plugin.getDescription().getName()+".yml");
-        //File f = new File("lol.yml");
         YamlConfiguration yaml = new YamlConfiguration();
         try {
             if (!f.exists()) f.createNewFile();
@@ -50,9 +79,13 @@ public class IPMAPI {
         return yaml;
     }
     
-    public boolean saveConfig(Plugin plugin, YamlConfiguration yaml) {
+    /**
+     * Saves the config
+     * @param yaml YamlConfiguration to save
+     * @return boolean True for success otherwise false
+     */
+    public boolean saveConfig(YamlConfiguration yaml) {
         File f = new File(IPM.getDataFolder(), "/config/"+plugin.getDescription().getName()+".yml");
-        //File f = new File("lol.yml");
         try {
             yaml.save(f);
         } catch (IOException ex) {
@@ -61,39 +94,76 @@ public class IPMAPI {
         return true;
     }
     
+    /**
+     * Registers your API under your plugins name
+     * @param api Custom API to assign
+     */
     public void registerAPI(API api) {
         registerAPI(plugin.getDescription().getName(), api);
     }
     
+    /**
+     * Registers your API and assigns your custom name
+     * @param alias Name under which you API will be assigned
+     * @param api Custom API to assign
+     */
     public void registerAPI(String alias, API api) {
         IPM.getApis().put(alias, api);
     }
     
+    /**
+     * Get API of registered plugins
+     * @param pName Name of the plugin to search for
+     * @return API Returns the API, if plugin not found returns null
+     */
     public API getAPI(String pName) {
         return IPM.getApis().get(pName);
     }        
     
+    /**
+     * Add one item to the plugins database
+     * @param category Category to find the item
+     * @param key Key to find the item
+     * @param value Value to assign to the key and category
+     * @return boolean True for success otherwise false
+     */
     public boolean putData(String category, String key, Object value) {
         return config.getStHandler().put(plugin.getDescription().getName(), category, key, value);
     }
     
+    /**
+     * Remove one item out of the database
+     * @param category Category to look for
+     * @param key Key to look for
+     * @return boolean True for success otherwise false
+     */
     public boolean removeData(String category, String key) {
         return config.getStHandler().remove(plugin.getDescription().getName(), category, key);
     }
     
+    /**
+     * Get one item out of the database
+     * @param category Category to look for
+     * @param key Key to look for
+     * @return Object Return the object specified by the category and key
+     */
     public Object getData(String category, String key) {
         return config.getStHandler().get(plugin.getDescription().getName(), category, key);
     }
     
     /**
-     * Pointless ^^
-     * @param value 
-     * @return value
+     * Pointless ^^ Returns argument
+     * @param value The value to return
+     * @return value Returns the parameter value
      */
     public Object getData(Object value) {
         return value;
     }
     
+    /**
+     * Get the IndiPlex-Manager database connection
+     * @return Connection The database connection 
+     */
     public Connection getDBConnection() {
         return config.getStHandler().getConnection();
     }
--- src/de/indiplex/manager/IPMPlugin.java
+++ src/de/indiplex/manager/IPMPlugin.java
@@ -1,3 +1,20 @@
+/*
+ * IndiPlexManager
+ * Copyright (C) 2011 IndiPlex
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.indiplex.manager;
 
 import de.indiplex.manager.util.Version;
@@ -5,13 +22,13 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- *
- * @author IndiPlex
+ * 
+ * @author IndiPlex <Cartan12@indiplex.de>
  */
 public abstract class IPMPlugin extends JavaPlugin {
     
     private IPMPluginInfo info;
-    private static IPMAPI API; //TODO: Maybe the API should be static and every class in the plugin can use: (e.g.) IPMAPI api = MyIPMPlugin.getIPMAPI();
+    private static IPMAPI API;
     protected boolean online = true;
     public static final Logger log = Logger.getLogger("Minecraft");
 
